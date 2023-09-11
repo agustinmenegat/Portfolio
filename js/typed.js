c71bb653a3a804480703569b8b603fe0baf83f6d
agustinmenegat <agusmenegat@hotmail.com>
2023-09-11 11:44:41 -0300

    video

--- js/typed.js
+++ js/typed.js
@@ -1,5 +1,18 @@
-const typed = new Typed('#typed',{
-    strings:['AGUSMENEGAT</br>@GMAIL.COM'],
-    typeSpeed: 75,
-    startDelay: 300
-})
+function iniciarAnimacionTyped() {
+  const opcionesTyped = {
+    strings: ['AGUSMENEGAT</br>@GMAIL.COM'],
+    typeSpeed: 100,
+    startDelay: 300,
+    onComplete: function (self) {
+      setTimeout(function () {
+        self.reset(); // Reiniciar la animación
+        self.start(); // Comenzar la animación nuevamente
+      }, 2000); // Reiniciar la animación después de 1 segundo
+    },
+  };
+
+  const typed = new Typed('#typed', opcionesTyped);
+}
+
+// Llama a la función para iniciar la animación Typed al cargar la página
+document.addEventListener('DOMContentLoaded', iniciarAnimacionTyped);
